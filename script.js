1efd794b11b5eeedfed8f4d37a0b1088a0912e61
Gautham Baiju <gauthambaiju040@gmail.com>
2025-07-04 22:41:17 +0530

    added filter functiionalities

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 let data = null;
 let displayData = [];
+let displayDataCopy = [];
 let filters = {
-    stars: 0,
+    stars: 1,
     brands: new Set(),
-    price: [165, 145500],
+    price: [165, 145100],
 };
+const starsMap = {
+    "1.0": "-18px -400px",
+    1.5: "-18px -400px",
+    "2.0": "-18px -400px",
+    2.5: "-34px -400px",
+    "3.0": "-18px -400px",
+    3.5: "-18px -400px",
+    "4.0": "-210px -468px",
+    4.5: "-2px -400px",
+    "5.0": "-194px -468px",
+};
+
+const minPriceSlider = document.querySelector(
+    ".main-content .left .price-slider .lower"
+);
+const maxPriceSlider = document.querySelector(
+    ".main-content .left .price-slider .upper"
+);
+
+const minPriceLabel = document.querySelector(
+    ".main-content .left .price-container #min-price"
+);
+const maxPriceLabel = document.querySelector(
+    ".main-content .left .price-container #max-price"
+);
+
 const brands = document.querySelectorAll(".main-content .left .brand-checkbox");
 
 const container = document.querySelector(
@@ -33,46 +60,7 @@ const fives = document.querySelectorAll(
     ".main-content .rating-container .five"
 );
 
-ones.forEach((element) => {
-    element.addEventListener("click", () => {
-        stars.forEach((star) => {
-            star.style.display = "none";
-        });
-        oneStar.style.display = "block";
-    });
-});
-twos.forEach((element) => {
-    element.addEventListener("click", () => {
-        stars.forEach((star) => {
-            star.style.display = "none";
-        });
-        twoStar.style.display = "block";
-    });
-});
-threes.forEach((element) => {
-    element.addEventListener("click", () => {
-        stars.forEach((star) => {
-            star.style.display = "none";
-        });
-        threeStar.style.display = "block";
-    });
-});
-fours.forEach((element) => {
-    element.addEventListener("click", () => {
-        stars.forEach((star) => {
-            star.style.display = "none";
-        });
-        fourStar.style.display = "block";
-    });
-});
-fives.forEach((element) => {
-    element.addEventListener("click", () => {
-        stars.forEach((star) => {
-            star.style.display = "none";
-        });
-        fiveStar.style.display = "block";
-    });
-});
+let middlePart = "";
 
 const firstPart = `<div class="desc">
                         <h2>Results</h2>
@@ -107,8 +95,6 @@ const lastPart = `<div class="pagination-container">
                         </div>
                     </div>`;
 
-let middlePart = "";
-
 (async function () {
     try {
         const response = await fetch("data.json");
@@ -118,40 +104,13 @@ let middlePart = "";
     }
 
     for (let key in data) {
-        data[key].forEach((productDetails) => {
-            displayData.push(productDetails);
-        });
+        displayData.push(...data[key]);
+        displayDataCopy.push(...data[key]);
     }
-    setData();
-    container.innerHTML = `${firstPart}${middlePart}${lastPart}`;
 
-    brands.forEach((brand) => {
-        brand.addEventListener("change", (event) => {
-            if (event.target.checked) {
-                filters.brands.add(event.target.value);
-            } else {
-                filters.brands.delete(event.target.value);
-            }
-            if (filters.brands.size > 0) {
-                displayData = [];
-                // try debugging here
-                for (let brand of filters.brands) {
-                    data[brand].forEach((productDetails) => {
-                        displayData.push(productDetails);
-                    });
-                }
-            }
-            console.log("Workingggg");
-            console.log(filters.brands);
-            middlePart = "";
-            setData();
-            container.innerHTML = `${firstPart}${middlePart}${lastPart}`;
-        });
-    });
-
-    function setData() {
-        for (let i of displayData) {
-            const {
+    function setDisplayData() {
+        for (let productDetails of displayData) {
+            let {
                 title,
                 numReviews,
                 numBuys,
@@ -161,7 +120,16 @@ let middlePart = "";
                 isPrime,
                 imagePath,
                 rating,
-            } = i;
+            } = productDetails;
+
+            numReviews = Number(numReviews).toLocaleString();
+            discountedPrice = Number(discountedPrice);
+            originalPrice = Number(originalPrice);
+            let discountPercentage = Math.ceil(
+                ((originalPrice - discountedPrice) / originalPrice) * 100
+            );
+            discountedPrice = discountedPrice.toLocaleString();
+            originalPrice = originalPrice.toLocaleString();
 
             middlePart += `<div class="card-container">
                         <div class="card-container-2">
@@ -178,7 +146,9 @@ let middlePart = "";
                                     <div class="review-container">
                                         <div class="product-stars-wrapper">
                                             <a class="product-stars">
-                                                <i class="star-img"></i>
+                                                <i class="star-img" style="background-position: ${
+                                                    starsMap[rating]
+                                                };"></i>
                                                 <i class="down-arrow"></i>
                                             </a>
                                             <a class="review-number">${numReviews}</a>
@@ -191,22 +161,23 @@ let middlePart = "";
                                         <div class="price-wrapper">
                                             <div class="price-container">
                                                 <span id="p1">&#8377;</span>
-                                                <span id="p2">${Number(
-                                                    discountedPrice
-                                                ).toLocaleString()}</span>
+                                                <span id="p2">${discountedPrice}</span>
                                                 <span id="p3">M.R.P:</span>
-                                                <span id="p4">&#8377;${Number(
-                                                    originalPrice
-                                                ).toLocaleString()}</span>
+                                                <span id="p4">&#8377;${originalPrice}</span>
                                                 <span id="space"></span>
-                                                <span id="p5">(35% off)</span>
+                                                <span id="p5">(${discountPercentage}% off)</span>
                                             </div>
                                             <span>Up to 5% back with Amazon Pay ICICI card</span>
                                         </div>
                                         <div class="delivery-wrapper">
-                                            <div class="prime-logo-container">
-                                                <i class="prime-logo"></i>
-                                            </div>
+                                            ${
+                                                isPrime
+                                                    ? `<div class="prime-logo-container">
+                                                            <i class="prime-logo"></i>
+                                                        </div>`
+                                                    : ``
+                                            }
+                                            
                                             <div class="delivery-details d1">
                                                 <span class="text-1">${
                                                     deliveryDetails[0]
@@ -235,5 +206,108 @@ let middlePart = "";
         }
     }
 
-    // console.log(JSON.stringify(displayData, null, 2));
+    function filterDisplayData() {
+        displayData = [...displayDataCopy];
+
+        if (filters.brands.size > 0) {
+            displayData = [];
+            for (let brand of filters.brands) {
+                if (data[brand]) {
+                    displayData.push(...data[brand]);
+                }
+            }
+        }
+
+        displayData = displayData.filter(
+            (productDetails) => Number(productDetails.rating) >= filters.stars
+        );
+
+        displayData = displayData.filter((productDetails) => {
+            return (
+                Number(productDetails.discountedPrice) >= filters.price[0] &&
+                Number(productDetails.discountedPrice) <= filters.price[1]
+            );
+        });
+    }
+
+    function updateDisplayData() {
+        middlePart = "";
+        filterDisplayData();
+        setDisplayData();
+        container.innerHTML = `${firstPart}${middlePart}${lastPart}`;
+    }
+
+    updateDisplayData();
+
+    brands.forEach((brand) => {
+        brand.addEventListener("change", (event) => {
+            if (event.target.checked) {
+                filters.brands.add(event.target.value);
+            } else {
+                filters.brands.delete(event.target.value);
+            }
+            updateDisplayData();
+        });
+    });
+
+    ones.forEach((element) => {
+        element.addEventListener("click", () => {
+            stars.forEach((star) => (star.style.display = "none"));
+            oneStar.style.display = "block";
+            filters.stars = 1;
+            updateDisplayData();
+        });
+    });
+    twos.forEach((element) => {
+        element.addEventListener("click", () => {
+            stars.forEach((star) => (star.style.display = "none"));
+            twoStar.style.display = "block";
+            filters.stars = 2;
+            updateDisplayData();
+        });
+    });
+    threes.forEach((element) => {
+        element.addEventListener("click", () => {
+            stars.forEach((star) => (star.style.display = "none"));
+            threeStar.style.display = "block";
+            filters.stars = 3;
+            updateDisplayData();
+        });
+    });
+    fours.forEach((element) => {
+        element.addEventListener("click", () => {
+            stars.forEach((star) => (star.style.display = "none"));
+            fourStar.style.display = "block";
+            filters.stars = 4;
+            updateDisplayData();
+        });
+    });
+    fives.forEach((element) => {
+        element.addEventListener("click", () => {
+            stars.forEach((star) => (star.style.display = "none"));
+            fiveStar.style.display = "block";
+            filters.stars = 5;
+            updateDisplayData();
+        });
+    });
+
+    minPriceSlider.addEventListener("input", (event) => {
+        minPriceLabel.textContent = `₹${event.target.value.toLocaleString(
+            "en-US"
+        )}`;
+        filters.price[0] = Number(event.target.value);
+    });
+    maxPriceSlider.addEventListener("input", (event) => {
+        maxPriceLabel.textContent = `₹${event.target.value.toLocaleString(
+            "en-US"
+        )}`;
+        filters.price[1] = Number(event.target.value);
+    });
+
+    minPriceSlider.addEventListener("change", () => {
+        updateDisplayData();
+    });
+    maxPriceSlider.addEventListener("change", () => {
+        updateDisplayData();
+    });
 })();
