e5b38484bb14d981563e36555bbb19b460356098
Gautham Baiju <gauthambaiju040@gmail.com>
2025-07-06 00:16:19 +0530

    added footer section

--- script.js
+++ script.js
@@ -24,6 +24,9 @@ const minPriceSlider = document.querySelector(
 const maxPriceSlider = document.querySelector(
     ".main-content .left .price-slider .upper"
 );
+const sliderButton = document.querySelector(
+    ".main-content .left .price-slider .slider-button span"
+);
 
 const minPriceLabel = document.querySelector(
     ".main-content .left .price-container #min-price"
@@ -68,32 +71,52 @@ const firstPart = `<div class="desc">
                     </div>`;
 
 const lastPart = `<div class="pagination-container">
-                        <div class="pagination-strip">
-                            <div class="previous disabled">
-                                <p>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewBox="0 0 8 12" focusable="false" aria-hidden="true"><path d="M5.874.35a1.28 1.28 0 011.761 0 1.165 1.165 0 010 1.695L3.522 6l4.113 3.955a1.165 1.165 0 010 1.694 1.28 1.28 0 01-1.76 0L0 6 5.874.35z"></path></svg>
-                                    Previous
-                                </p>
-                            </div>
-                            <div class="selected">1</div>
-                            <div>2</div>
-                            <div>3</div>
-                            <div class="disabled">...</div>
-                            <div class="disabled">33</div>
-                            <div class="next">
-                                <p>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewBox="0 0 8 12" focusable="false" aria-hidden="true"><path d="M2.126.35a1.28 1.28 0 00-1.761 0 1.165 1.165 0 000 1.695L4.478 6 .365 9.955a1.165 1.165 0 000 1.694 1.28 1.28 0 001.76 0L8 6 2.126.35z"></path></svg>
-                                    Next
-                                </p>
-                            </div>
+                    <div class="pagination-wrapper">
+                        <div class="pagination-wrapper-2">
+                            <span class="pagination-strip">
+                                <ul>
+                                    <span class="previous disabled fix">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewBox="0 0 8 12" focusable="false" aria-hidden="true"><path d="M5.874.35a1.28 1.28 0 011.761 0 1.165 1.165 0 010 1.695L3.522 6l4.113 3.955a1.165 1.165 0 010 1.694 1.28 1.28 0 01-1.76 0L0 6 5.874.35z"></path></svg>
+                                        Previous
+                                    </span>
+                                    <li class="selected fix2">
+                                        <span>
+                                            <span class="fi">1</span>
+                                        </span>
+                                    </li>
+                                    <li class="fix2">
+                                        <span>
+                                            <a href="" class="fi">2</a>
+                                        </span>
+                                    </li>
+                                    <li class="fix2">
+                                        <span>
+                                            <a href="" class="fi">3</a>
+                                        </span>
+                                    </li>
+                                    <span class="disabled ellipsis fix2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" viewBox="0 0 10 2" aria-hidden="true"><path d="M9 2c-.608 0-1-.425-1-1s.392-1 1-1 1 .448 1 1c0 .575-.392 1-1 1zM5 2c-.608 0-1-.425-1-1s.392-1 1-1 1 .448 1 1c0 .575-.392 1-1 1zM1 2c-.608 0-1-.425-1-1s.392-1 1-1 1 .448 1 1c0 .575-.392 1-1 1z"></path>...</svg>
+                                    </span>
+                                    <span class="disabled fix">33</span>
+                                    <li class="next">
+                                        <span>
+                                            <a href="" class="fix">
+                                                Next
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewBox="0 0 8 12" focusable="false" aria-hidden="true"><path d="M2.126.35a1.28 1.28 0 00-1.761 0 1.165 1.165 0 000 1.695L4.478 6 .365 9.955a1.165 1.165 0 000 1.694 1.28 1.28 0 001.76 0L8 6 2.126.35z"></path></svg>
+                                            </a>
+                                        </span>
+                                    </li>
+                                </ul>
+                            </span>
                         </div>
                     </div>
-                    <div class="help-container">
-                        <div class="help">
-                            <h2>Need help?</h2>
-                            <p>Visit the help section or contact us</p>
-                        </div>
-                    </div>`;
+                </div>
+                <div class="help-container">
+                    <div class="help">
+                        <h2>Need help?</h2>
+                        <p>Visit the help section <span style="color: black;">or</span> contact us</p>
+                    </div>
+                </div>`;
 
 (async function () {
     try {
@@ -241,10 +264,14 @@ const lastPart = `<div class="pagination-container">
 
     brands.forEach((brand) => {
         brand.addEventListener("change", (event) => {
+            console.log("workinggggg");
+            const label = brand.nextElementSibling;
             if (event.target.checked) {
                 filters.brands.add(event.target.value);
+                label.style.fontWeight = "700";
             } else {
                 filters.brands.delete(event.target.value);
+                label.style.fontWeight = "normal";
             }
             updateDisplayData();
         });
@@ -303,11 +330,5 @@ const lastPart = `<div class="pagination-container">
         )}`;
         filters.price[1] = Number(event.target.value);
     });
-
-    minPriceSlider.addEventListener("change", () => {
-        updateDisplayData();
-    });
-    maxPriceSlider.addEventListener("change", () => {
-        updateDisplayData();
-    });
+    sliderButton.addEventListener("click", () => updateDisplayData());
 })();
